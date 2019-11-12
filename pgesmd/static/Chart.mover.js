1576286f0d8df1a5d770d43cc2fcc9d6c720afa9
J.P. Hutchins <jphutchins@gmail.com>
2019-11-12 15:04:13 -0800

    Fix scroller going out of index bounds

--- pgesmd/static/Chart.mover.js
+++ pgesmd/static/Chart.mover.js
@@ -1,4 +1,4 @@
-function findIndex(dates, date) {
+function findEspiIndex(dates, date) {
   let start = 0, end = dates.length-1, mid = 0;
 
   while (start <= end) {
@@ -14,26 +14,42 @@ function getSlicePoints(startId, endId) {
   var startDate = moment(document.getElementById(startId).value).valueOf();
   var endDate = moment(document.getElementById(endId).value).valueOf();
   var result = {
-    start: findIndex(dates, startDate) + 1,
-    end: findIndex(dates, endDate)
+    start: findEspiIndex(dates, startDate) + 1,
+    end: findEspiIndex(dates, endDate)
   };
   return result;
 }
   
 function previousInterval(interval, chart, dates, lookup) {
   var startDate = chart.data.datasets[0].data[0]["x"];
+  var firstEntry = chart.data.db_info.first_entry * 1000;
+
+  var start = Math.max(firstEntry, moment(startDate).add(-1, interval).valueOf());
+  var end = moment(start).add(1, interval).valueOf();
+  
+  start = findEspiIndex(dates, start);
+  end = findEspiIndex(dates, end);
+
   var result = {
-    start: findIndex(dates, moment(startDate).add(-1, interval).valueOf()),
-    end: lookup[startDate]
+    start: start,
+    end: end
   };
   return result;
 }
 
 function nextInterval(interval, chart, dates, lookup) {
   var startDate = chart.data.datasets[0].data[chart.data.datasets[0].data.length - 1]["x"];
+  var lastEntry = chart.data.db_info.last_entry * 1000;
+
+  var end = Math.min(lastEntry, moment(startDate).add(1, interval).valueOf());
+  var start = moment(end).add(-1, interval).valueOf();
+
+  start = findEspiIndex(dates, start) + 1;
+  end = findEspiIndex(dates, end) + 1;
+  
   var result = {
-    start: lookup[startDate] + 1,
-    end: findIndex(dates, moment(startDate).add(1, interval).valueOf()) + 1
+    start: start,
+    end: end
   };
   return result;
 }
@@ -41,4 +57,23 @@ function nextInterval(interval, chart, dates, lookup) {
 function displayDates(i, j) {
   document.getElementById("firstChartDate").innerHTML = info[i].date;
   document.getElementById("lastChartDate").innerHTML = info[j].date;
+}
+
+function displayDates2(i, j) {
+  document.getElementById("firstChartDate").innerHTML = info[i].date;
+  document.getElementById("lastChartDate").innerHTML = info[j].date;
+}
+
+function getLatestWeeksData(part_data, db_info) {
+  var i = part_data.length - 1;
+  while (moment(part_data[i]["x"]).format('dddd') != 'Friday') {
+    i--;
+  };
+  end = i + 1;
+  start = findEspiIndex(dates, moment(part_data[end]["x"]).add(-1, "w").valueOf());
+  var result = {
+    start: start,
+    end: end
+  };
+  return result;
 }
