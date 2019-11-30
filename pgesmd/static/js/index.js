3bed0f68e7c4491d6709c7e4181a02d8c8818f46
J.P. Hutchins <jphutchins@gmail.com>
2019-11-30 15:49:03 -0800

    Test Immutable.js

--- pgesmd/static/js/index.js
+++ pgesmd/static/js/index.js
@@ -1,2 +1,8 @@
 import Chart from 'chart.js';
 import Moment from 'moment';
+
+const { Map } = require('immutable');
+
+const map1 = Map({ a: 1, b: 2});
+const map2 = map1.set('b', 50);
+console.log(map1.get('b') + " vs. " + map2.get('b'));
