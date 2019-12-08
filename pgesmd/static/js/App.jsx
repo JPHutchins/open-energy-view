19731949ef272f78556438640b90cf203e083aea
J.P. Hutchins <jphutchins@gmail.com>
2019-12-07 23:50:42 -0800

    Add App.jsx

---
+++ pgesmd/static/js/App.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import EnergyHistory from "./components/energyHistory"
+
+export default class App extends React.Component {
+  render () {
+    return < EnergyHistory />;
+  }
+}
