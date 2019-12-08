58381a12608b4fb5ffe6218497d54c30df0f6bd9
J.P. Hutchins <jphutchins@gmail.com>
2019-12-07 23:52:12 -0800

    Add EnergyChart (child)

---
+++ pgesmd/static/js/components/energyChart.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import {Bar} from 'react-chartjs-2';
+
+export default class EnergyChart extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  
+
+  render() {
+    return (
+      <div>
+        {/* <button 
+          onClick={this.handleEvening}
+          className='btn'
+        >
+          Evening Only
+        </button> */}
+        
+        <h2>Energy History</h2>
+        <Bar data={this.props.data} options={this.props.options} />
+      </div>
+    );
+  }
+};
