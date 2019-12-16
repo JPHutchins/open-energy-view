e1c4cfafa679da1683fd077b83ee28dfb56ade3a
J.P. Hutchins <jphutchins@gmail.com>
2019-12-15 17:22:20 -0800

    Handle zooming

--- pgesmd/static/js/components/energyHistory.jsx
+++ pgesmd/static/js/components/energyHistory.jsx
@@ -1,154 +1,222 @@
 import React from "react";
-import EnergyChart from "./energyChart"
-import { getCompleteData, makeOptions } from '../utils.js';
+import moment from "moment";
+import EnergyChart from "./energyChart";
+import { getCompleteData, makeOptions } from "../utils.js";
+import "../../css/App.css";
 
-const { fromJS } = require('immutable');
+const { fromJS } = require("immutable");
 
 export default class EnergyHistory extends React.Component {
   constructor(props) {
     super(props);
-    this.colors = ['#0000A0', '#add8e6', '#800080'];
+    this.colors = ["#0000A0", "#add8e6", "#800080"];
     this.state = {
-      data: {}
+      data: {},
+      description: {
+        startDate: "",
+        endDate: ""
+      }
     };
     this.indexReference = {
-      hour: 'day',
-      part: 'week',
-      day: 'month',
-      week: 'year',
-      month: 'year'
-    }
+      hour: "day",
+      part: "week",
+      day: "month",
+      week: "year",
+      month: "year",
+      year: "all"
+    };
+    this.zoom = ["hour", "part", "day", "week", "month", "year"];
     this.chartSettings = {
       barPercentage: 1.0,
-      barThickness: 'flex'
-    }
+      barThickness: "flex"
+    };
   }
 
   componentDidMount = () => {
     getCompleteData()
       .then(data => {
         this.database = fromJS(data);
+        console.log(this.database.toJS());
       })
       .then(() => {
         this.setInit();
-      })
-  }
+      });
+  };
 
   setInit = () => {
-    const parts = this.database.get('part');
-    const weeks = this.database.get('week');
+    const parts = this.database.get("part");
+    const weeks = this.database.get("week");
 
-    const lo = weeks.get(weeks.size - 1).get('i_part_start');
-    const hi = weeks.get(weeks.size - 1).get('i_part_end');
+    const lo = weeks.get(weeks.size - 1).get("i_part_start");
+    const hi = weeks.get(weeks.size - 1).get("i_part_end");
 
-    const newData = parts.slice(lo - 1, hi);  // this OFF BY ONE is an error and should be corrected in database.py
+    const newData = parts.slice(lo - 1, hi); // this OFF BY ONE is an error and should be corrected in database.py
 
-    const partColors = newData.map(item => 
-      this.colors[item.get('part')]
-      );
+    const partColors = newData.map(item => this.colors[item.get("part")]);
 
     this.setState({
       data: {
-        datasets: [{
-          data: newData.toJS(),
-          backgroundColor: partColors.toJS(),
-          barPercentage: this.chartSettings.barPercentage,
-          barThickness: this.chartSettings.barThickness
-        }]
+        datasets: [
+          {
+            data: newData.toJS(),
+            backgroundColor: partColors.toJS(),
+            barPercentage: this.chartSettings.barPercentage,
+            barThickness: this.chartSettings.barThickness
+          }
+        ]
       },
-      options: makeOptions(newData.toJS()[0]['type'])
+      options: makeOptions(newData.toJS()[0]["type"]),
+      description: this.createDescription(newData.toJS())
     });
-  }
+  };
 
-  handleScroll = (direction) => {
-    const modifyRange = direction === 'previous' ? -1 : 1;
+  handleScroll = direction => {
+    const modifyRange = direction === "previous" ? -1 : 1;
 
     const currentData = this.state.data.datasets[0].data;
     const type = currentData[0].type;
     const superType = this.indexReference[type];
-  
+
     const dataPoints = this.database.get(type);
     const dataRange = this.database.get(superType);
 
-    const lo = dataRange
-                .get(currentData[0]["i_" + superType] + modifyRange)
-                .get('i_' + type + '_start');
-    const hi = dataRange
-                .get(currentData[0]["i_" + superType] + modifyRange)
-                .get('i_' + type + '_end');
-    const newData = dataPoints.slice(lo, hi);  // this OFF BY ONE is an error and should be corrected in database.py
+    const newSuperIndex = currentData[0]["i_" + superType] + modifyRange;
 
-    const partColors = newData.map(item => 
-      this.colors[item.get(type)]
-      );
+    const lo = dataRange.get(newSuperIndex).get("i_" + type + "_start");
+    const hi = dataRange.get(newSuperIndex).get("i_" + type + "_end");
+    const newData = dataPoints.slice(lo, hi); // this OFF BY ONE is an error and should be corrected in database.py
+
+    const partColors = newData.map(item => this.colors[item.get(type)]);
 
     this.setState({
       data: {
-        datasets: [{
-          data: newData.toJS(),
-          backgroundColor: partColors.toJS(),
-          barPercentage: this.chartSettings.barPercentage,
-          barThickness: this.chartSettings.barThickness
-        }]
+        datasets: [
+          {
+            data: newData.toJS(),
+            backgroundColor: partColors.toJS(),
+            barPercentage: this.chartSettings.barPercentage,
+            barThickness: this.chartSettings.barThickness
+          }
+        ]
       },
-      options: makeOptions(type)
+      options: makeOptions(type),
+      description: this.createDescription(newData.toJS())
     });
-  }
-
-
+  };
 
   handlePrevious = () => {
-    this.handleScroll('previous');
-  } 
+    this.handleScroll("previous");
+  };
 
   handleNext = () => {
-    this.handleScroll('next');
-  } 
+    this.handleScroll("next");
+  };
 
-  handleEvening = () => {
-    const userColors = ['#0000A0', '#add8e6', '#800080'];
-    const newData = this.state.data.datasets[0].data.filter(item => item.part === 0);
+  handleZoomOut = () => {
+    const currentData = this.state.data.datasets[0].data;
+    const currentType = currentData[0].type;
+    const type = this.zoom[this.zoom.indexOf(currentType) + 1];
+
+    const superType = this.indexReference[type];
+
+    const dataPoints = this.database.get(type);
+
+    let dataRange;
+    let newData;
+
+    if (type === "month") {
+      dataRange = this.database.get("month");
+      newData = dataPoints;
+    } else {
+      dataRange = this.database.get(superType);
+      const lo = dataRange
+        .get(currentData[0]["i_" + superType])
+        .get("i_" + type + "_start");
+      const hi = dataRange
+        .get(currentData[0]["i_" + superType])
+        .get("i_" + type + "_end");
+
+      newData = dataPoints.slice(lo, hi);
+    }
+
     this.setState({
       data: {
-        datasets: [{
-          data: newData,
-          backgroundColor: userColors[0],
-          barPercentage: 1.1,
-          barThickness: 'flex'
-        }]
+        datasets: [
+          {
+            data: newData.toJS(),
+            backgroundColor: "#0000A0",
+            barPercentage: this.chartSettings.barPercentage,
+            barThickness: this.chartSettings.barThickness
+          }
+        ]
       },
-      i_intvl: 'i_week',
+      options: makeOptions(type),
+      description: this.createDescription(newData.toJS())
     });
-  }
+  };
 
-  render () {
-    return <div>
-            <EnergyChart
-            data={this.state.data}
-            options={this.state.options}
-            colors={this.colors}
-            database={this.database} />
-          
-          <button 
-            onClick={this.handlePrevious}
-            className='btn'
-            style={{
-              display: 'flex',
-              justifyContent: 'center',
-            }}
-          >
-            Previous
-          </button>
-          <button 
-            onClick={this.handleNext}
-            className='btn'
-            style={{
-              display: 'flex',
-              justifyContent: 'center',
-            }}
-          >
-            Next
-          </button>
-          </div>;
+  handleEvening = () => {
+    const userColors = ["#0000A0", "#add8e6", "#800080"];
+    const newData = this.state.data.datasets[0].data.filter(
+      item => item.part === 0
+    );
+    this.setState({
+      data: {
+        datasets: [
+          {
+            data: newData,
+            backgroundColor: userColors[0],
+            barPercentage: 1.1,
+            barThickness: "flex"
+          }
+        ]
+      },
+      i_intvl: "i_week"
+    });
+  };
+
+  createDescription = currentData => {
+    const startDate = moment(currentData[0].interval_start).format(
+      "MMMM Do, YYYY"
+    );
+    const endDate = moment(
+      currentData[currentData.length - 1].interval_end
+    ).format("MMMM Do, YYYY");
+    return {
+      startDate: startDate,
+      endDate: endDate
+    };
+  };
+
+  render() {
+    return (
+      <div>
+        <EnergyChart
+          data={this.state.data}
+          options={this.state.options}
+          colors={this.colors}
+          database={this.database}
+        />
+        <div className="container">
+          <b className="box">
+            <span>{this.state.description.startDate}</span>
+          </b>
+          <div className="box">
+            <button onClick={this.handlePrevious} className="btn">
+              Previous
+            </button>
+            <button onClick={this.handleNext} className="btn">
+              Next
+            </button>
+          </div>
+          <b className="box">
+            <span>{this.state.description.endDate}</span>
+          </b>
+        </div>
+        <button onClick={this.handleZoomOut} className="btn">
+          Zoom Out
+        </button>
+      </div>
+    );
   }
 }
