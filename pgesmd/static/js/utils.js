96e45e38df83f3470cb1ab1e0eec10d0775867b7
J.P. Hutchins <jphutchins@gmail.com>
2019-12-15 16:27:28 -0800

    Prettier formatting

--- pgesmd/static/js/utils.js
+++ pgesmd/static/js/utils.js
@@ -1,78 +1,82 @@
 export function getPromise() {
-    return new Promise(function(resolve, reject) {
-        const request = new XMLHttpRequest();
-        request.open("GET", '/data/json');
-        request.onreadystatechange = function () {
-            if(request.readyState === XMLHttpRequest.DONE && request.status === 200) {
-                try {
-                    resolve(JSON.parse(request.responseText));
-                } catch(e) {
-                    reject(e);
-                }
-            }
-        };
-        request.send();
-    })
+  return new Promise(function(resolve, reject) {
+    const request = new XMLHttpRequest();
+    request.open("GET", "/data/json");
+    request.onreadystatechange = function() {
+      if (
+        request.readyState === XMLHttpRequest.DONE &&
+        request.status === 200
+      ) {
+        try {
+          resolve(JSON.parse(request.responseText));
+        } catch (e) {
+          reject(e);
+        }
+      }
+    };
+    request.send();
+  });
 }
 
 export function makeOptions(type) {
-    const unit = {
-        hour: 'hour',
-        part: 'day',
-        day: 'day',
-        week: 'day',
-        month: 'month'
-    }
-    const options = {
-        scales: {
-            xAxes: [{
-                type: 'time',
-                bounds: 'ticks',
-                time: {
-                unit: unit[type],
-                displayFormats: {
-                    minute: 'hA',
-                    hour: 'hA',
-                    day: 'ddd'
-                    },
-                },
-            offset: false
-            }]
+  const unit = {
+    hour: "hour",
+    part: "day",
+    day: "day",
+    week: "day",
+    month: "month"
+  };
+  const options = {
+    scales: {
+      xAxes: [
+        {
+          type: "time",
+          bounds: "ticks",
+          time: {
+            unit: unit[type],
+            displayFormats: {
+              minute: "hA",
+              hour: "hA",
+              day: "ddd"
+            }
+          },
+          offset: false
         }
-        
+      ]
     }
-    return options;
+  };
+  return options;
 }
 
 export function getDataNow() {
-    const request = new XMLHttpRequest();
-    request.open("GET", '/data/json/now', false);
-    request.send(null);
-    if (request.status === 200) {
-        try {
-            return JSON.parse(request.responseText);
-        } catch(e) {
-            console.log(e);
-        }
+  const request = new XMLHttpRequest();
+  request.open("GET", "/data/json/now", false);
+  request.send(null);
+  if (request.status === 200) {
+    try {
+      return JSON.parse(request.responseText);
+    } catch (e) {
+      console.log(e);
     }
+  }
 }
 
 export async function getCompleteData() {
-    let response = await fetch('/data/json');
-    let data = await response.json()
-    return data;
+  let response = await fetch("/data/json");
+  let data = await response.json();
+  return data;
 }
 
 function getEnergyHistory(promise) {
-    promise().then(function(text) {
-        return text;
-    }).catch(function(err) {
-        console.log(err);
+  promise()
+    .then(function(text) {
+      return text;
+    })
+    .catch(function(err) {
+      console.log(err);
     });
 }
 
-
-
 export function add(x, y) {
-    return x + y;
+  return x + y;
 }
