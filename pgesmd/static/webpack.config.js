6fc1c2afb208af5e64e028f8ed633c7f81138ac2
J.P. Hutchins <jphutchins@gmail.com>
2019-12-15 16:26:35 -0800

    Add style-loader and css-loader

--- pgesmd/static/webpack.config.js
+++ pgesmd/static/webpack.config.js
@@ -1,25 +1,29 @@
-const webpack = require('webpack');
+const webpack = require("webpack");
 const config = {
-    entry:  ['babel-polyfill', __dirname + '/js/index.jsx'],
-    output: {
-        path: __dirname + '/dist',
-        filename: 'bundle.js',
-    },
-    resolve: {
-        extensions: ['.js', '.jsx', '.css']
-    },
-    module: {
-        rules: [
+  entry: ["babel-polyfill", __dirname + "/js/index.jsx"],
+  output: {
+    path: __dirname + "/dist",
+    filename: "bundle.js"
+  },
+  resolve: {
+    extensions: [".js", ".jsx", ".css"]
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"]
+      },
+      {
+        test: /\.jsx?/,
+        exclude: /node_modules/,
+        use: [
           {
-            test: /\.jsx?/,
-            exclude: /node_modules/,
-            use: [
-              {
-                loader: 'babel-loader',
-              }
-            ]
+            loader: "babel-loader"
           }
         ]
-    }
+      }
+    ]
+  }
 };
 module.exports = config;
