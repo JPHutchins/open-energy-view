ef972fdb1d045ac0614dd08cc7eabc5066b40d78
J.P. Hutchins <jphutchins@gmail.com>
2019-12-22 11:37:51 -0800

    Clean

--- pgesmd/static/js/components/energyChart.jsx
+++ pgesmd/static/js/components/energyChart.jsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import {Bar} from 'react-chartjs-2';
+import React from "react";
+import { Bar } from "react-chartjs-2";
 
 export default class EnergyChart extends React.Component {
   constructor(props) {
@@ -10,16 +10,8 @@ export default class EnergyChart extends React.Component {
   render() {
     return (
       <div>
-        {/* <button 
-          onClick={this.handleEvening}
-          className='btn'
-        >
-          Evening Only
-        </button> */}
-        
-        <h2>Energy History</h2>
         <Bar data={this.props.data} options={this.props.options} />
       </div>
     );
   }
-};
+}
