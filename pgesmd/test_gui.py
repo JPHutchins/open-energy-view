644de4f72ede296a76611adc7e65b5cc34ef6556
J.P. Hutchins <jphutchins@gmail.com>
2020-01-04 13:47:23 -0800

    Remake database on test-gui

--- pgesmd/test_gui.py
+++ pgesmd/test_gui.py
@@ -9,6 +9,8 @@ PROJECT_PATH = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 def fill_database():
     """Fill database using the test data."""
+    if os.path.exists(f'{PROJECT_PATH}/test/data/energy_history_test.db'):
+        os.remove(f'{PROJECT_PATH}/test/data/energy_history_test.db')
     db = EnergyHistory(path='/test/data/energy_history_test.db')
     xml = f'{PROJECT_PATH}/test/data/espi/espi_2_years.xml'
     db.insert_espi_xml(xml)
