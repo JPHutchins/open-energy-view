a13cc8ad27dc52a8a87567c7e823c3cffaf6df5b
J.P. Hutchins <jphutchins@gmail.com>
2020-01-05 12:17:05 -0800

    Remove Redux from project

--- pgesmd/static/js/index.jsx
+++ pgesmd/static/js/index.jsx
@@ -1,15 +1,6 @@
 import React from "react";
 import { render } from "react-dom";
-import { Provider } from "react-redux";
 import App from "./App";
 import "../css/App.css";
-import configureStore from "./configureStore";
 
-const store = configureStore();
-
-render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  document.getElementById("root")
-);
+render(<App />, document.getElementById("root"));
