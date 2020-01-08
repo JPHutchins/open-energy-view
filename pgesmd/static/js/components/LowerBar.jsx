b6fecff5ee283515a177556e7679a212f8f4b664
J.P. Hutchins <jphutchins@gmail.com>
2020-01-07 18:19:21 -0800

    Change to react-bootstrap select

--- pgesmd/static/js/components/LowerBar.jsx
+++ pgesmd/static/js/components/LowerBar.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import LeftDate from "./LeftDate";
 import RightDate from "./RightDate";
 import "../../css/App.css";
+import { DropdownButton, Dropdown} from "react-bootstrap"
 
 export default class LowerBar extends React.PureComponent {
   render() {
@@ -17,13 +18,13 @@ export default class LowerBar extends React.PureComponent {
           >
             Previous
           </button>
-          <select className="form-control" onChange={this.props.onChange}>
-            <option>Day</option>
-            <option>Week</option>
-            <option>Month</option>
-            <option>Year</option>
-            <option>Complete</option>
-          </select>
+          <DropdownButton title={this.props.range} onSelect={this.props.onChange}>
+            <Dropdown.Item eventKey="Day">Day</Dropdown.Item>
+            <Dropdown.Item eventKey="Week">Week</Dropdown.Item>
+            <Dropdown.Item eventKey="Month">Month</Dropdown.Item>
+            <Dropdown.Item eventKey="Year">Year</Dropdown.Item>
+            <Dropdown.Item eventKey="Complete">Complete</Dropdown.Item>
+          </DropdownButton>
           <button
             onClick={() => this.props.onClick(1)}
             className="btn"
