7de0376c9861008346dca0659e0487a25cabbf8b
J.P. Hutchins <jphutchins@gmail.com>
2020-01-08 21:10:37 -0800

    Add simple react-bootstrap navbar

--- pgesmd/static/js/App.jsx
+++ pgesmd/static/js/App.jsx
@@ -1,8 +1,38 @@
 import React from "react";
-import EnergyHistory from "./components/energyHistory"
+import {
+  Navbar,
+  Nav,
+  NavDropdown,
+  Form,
+  FormControl,
+  Button
+} from "react-bootstrap";
+import EnergyHistory from "./components/energyHistory";
 
 export default class App extends React.Component {
-  render () {
-    return < EnergyHistory />;
+  render() {
+    return (
+      <>
+        <Navbar bg='dark' variant="dark">
+          <Navbar.Brand href="#home">Energy Monitor</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="mr-auto">
+              <Nav.Link>History</Nav.Link>
+              <Nav.Link>Insights</Nav.Link>
+            </Nav>
+            <Form inline>
+              <FormControl
+                type="text"
+                placeholder="Questions"
+                className="mr-sm-2"
+              />
+              <Button variant="outline-success">Search</Button>
+            </Form>
+          </Navbar.Collapse>
+        </Navbar>
+        <EnergyHistory />
+      </>
+    );
   }
 }
