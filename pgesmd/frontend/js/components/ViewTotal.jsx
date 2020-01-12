0e025753776f3956330c62a0b333508b3725e405
J.P. Hutchins <jphutchins@gmail.com>
2020-01-11 22:43:01 -0800

    Simplify total view

--- pgesmd/frontend/js/components/ViewTotal.jsx
+++ pgesmd/frontend/js/components/ViewTotal.jsx
@@ -2,22 +2,13 @@ import React from "react";
 
 const ViewTotal = props => {
   const percent = Math.round((props.sum / 1000 / props.avg - 1) * 100);
-
   const aboveOrBelow = percent <= 0 ? "below" : "above";
-
-  const upOrDown = percent <= 0 ? "arrow_downward" : "arrow_upward";
-
-  const greenOrOrangeBg =
-    percent <= 0 ? "background-green" : "background-orange";
-  const greenOrOrange = percent <= 0 ? "green" : "orange";
 
   return (
     <div>
-      <div className="kilowatt-hour">
+      <div className="kilowatt-hour">Total Used</div>
+      <div className="info-medium-number">
         {Math.round(props.sum / 1000)}kWh{"\n"}
-      </div>
-      <div className={greenOrOrange + " kilowatt-hour"}>
-        <i class="material-icons">{upOrDown}</i>
       </div>
       <div className="info-details">
         {Math.abs(percent)}% {aboveOrBelow} average
