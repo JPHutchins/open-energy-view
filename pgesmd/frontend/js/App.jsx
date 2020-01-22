583137724d2ddbe2313287762ca8eb2ff9cb1076
J.P. Hutchins <jphutchins@gmail.com>
2020-01-22 11:10:19 -0800

    Add DataRegistrationModal to the navbar

--- pgesmd/frontend/js/App.jsx
+++ pgesmd/frontend/js/App.jsx
@@ -8,18 +8,20 @@ import {
   Button
 } from "react-bootstrap";
 import EnergyHistory from "./components/energyHistory";
+import DataRegistrationModal from "./components/DataRegistrationModal";
 
 export default class App extends React.Component {
   render() {
     return (
       <>
-        <Navbar bg='dark' variant="dark">
+        <Navbar bg="dark" variant="dark">
           <Navbar.Brand href="#home">Energy Monitor</Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="mr-auto">
               <Nav.Link>History</Nav.Link>
               <Nav.Link>Insights</Nav.Link>
+              <DataRegistrationModal />
             </Nav>
             <Form inline>
               <FormControl
