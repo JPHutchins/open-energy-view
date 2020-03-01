816131e1e6226085a2661c1fafd112cda7022c54
J.P. Hutchins <jphutchins@gmail.com>
2020-02-29 20:44:41 -0800

    Comment CarbonFootprint

--- pgesmd/frontend/js/components/CarbonFootprint.jsx
+++ pgesmd/frontend/js/components/CarbonFootprint.jsx
@@ -1,5 +1,12 @@
 import React from "react";
 
+/**
+ * Returns a div displaying the lbs of carbon used during the view window.
+ *
+ * @param {Object} props React props.
+ * @param {Number} props.sum Total energy in Watts for view window.
+ * @param {Number} props.carbonMultiplier Utility provided estimated pounds of carbon per kWh of energy produced.
+ */
 const CarbonFootprint = props => {
   const carbon = Math.round((props.sum / 1000) * props.carbonMultiplier);
 
