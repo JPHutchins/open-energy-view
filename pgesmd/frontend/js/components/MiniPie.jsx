c93b2d3f8b4804c5af6cffb254abc5b913f1fcb1
J.P. Hutchins <jphutchins@gmail.com>
2020-03-01 19:39:17 -0800

    Comment MiniPie

--- pgesmd/frontend/js/components/MiniPie.jsx
+++ pgesmd/frontend/js/components/MiniPie.jsx
@@ -1,6 +1,13 @@
 import React from "react";
 import { Pie } from "react-chartjs-2";
 
+/**
+ * Return a flexibly sized ChartJS pie chart.
+ *
+ * @param {Object} props React props.
+ * @param {Object} props.data The ChartJS data object.
+ * @param {Object} props.options The ChartJS options object.
+ */
 const MiniPie = props => {
   return (
     <Pie data={props.data} options={props.options} height={null} width={null} />
