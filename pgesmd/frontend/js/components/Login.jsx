607eaf476ebf70bcd9db516e94bebe04da55a007
J.P. Hutchins <jphutchins@gmail.com>
2020-03-08 18:05:01 -0700

    Testing react-bootstrap login form

---
+++ pgesmd/frontend/js/components/Login.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import Form from "react-bootstrap";
+
+const Login = () => {
+  return (
+    <Form>
+      <Form.Group controlId="formBasicEmail">
+        <Form.Label>Email address</Form.Label>
+        <Form.Control type="email" placeholder="Enter email" />
+        <Form.Text className="text-muted">
+          We'll never share your email with anyone else.
+        </Form.Text>
+      </Form.Group>
+
+      <Form.Group controlId="formBasicPassword">
+        <Form.Label>Password</Form.Label>
+        <Form.Control type="password" placeholder="Password" />
+      </Form.Group>
+      <Button variant="primary" type="submit">
+        Submit
+      </Button>
+    </Form>
+  );
+};
+
+export default Login;
