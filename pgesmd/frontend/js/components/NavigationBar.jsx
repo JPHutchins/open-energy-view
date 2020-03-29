21da6c299be5dea67d7d963d57196a2ca1f8b75f
J.P. Hutchins <jphutchins@gmail.com>
2020-03-29 16:20:12 -0700

    Refactor Navbar into its own component

---
+++ pgesmd/frontend/js/components/NavigationBar.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { withRouter } from "react-router-dom";
+import { Navbar, Nav, NavDropdown } from "react-bootstrap";
+import AuthService from "../service/AuthService";
+import DataRegistrationModal from "./DataRegistrationModal";
+
+const NavigationBar = props => {
+  const handleLogout = () => {
+    AuthService.logOut()
+      .then(props.history.push("/login"))
+      .then(props.callback());
+  };
+
+  return (
+    <Navbar bg="dark" variant="dark">
+      <Navbar.Brand href="#home">Energy Monitor</Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="mr-auto">
+          <NavDropdown title="My Account" id="basic-nav-dropdown">
+            <NavDropdown.Divider />
+            <NavDropdown.Item onClick={handleLogout}>Logout</NavDropdown.Item>
+          </NavDropdown>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
+  );
+};
+
+export default withRouter(NavigationBar);
