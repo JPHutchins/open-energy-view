e1ef5cadff10658795f14509f80953472fa522a7
J.P. Hutchins <jphutchins@gmail.com>
2020-03-29 16:11:48 -0700

    Wrap App in BrowserRouter

--- pgesmd/frontend/js/index.jsx
+++ pgesmd/frontend/js/index.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { render } from "react-dom";
+import {BrowserRouter } from "react-router-dom"
 import App from "./App";
 import "../css/App.css";
 
-render(<App />, document.getElementById("root"));
+render(<BrowserRouter><App /></BrowserRouter>, document.getElementById("root"));
