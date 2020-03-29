d92e26935afa902276538b17837ebcd2c021df4c
J.P. Hutchins <jphutchins@gmail.com>
2020-03-29 16:08:37 -0700

    Comment out DB demo init, let's do manually if needed

--- pgesmd/test_gui.py
+++ pgesmd/test_gui.py
@@ -11,10 +11,10 @@ def fill_database():
     """Fill database using the test data."""
     if os.path.exists(f'{PROJECT_PATH}/test/data/energy_history_test.db'):
         os.remove(f'{PROJECT_PATH}/test/data/energy_history_test.db')
-    db = EnergyHistory(path='/test/data/energy_history_test.db')
-    xml = f'{PROJECT_PATH}/test/data/espi/espi_2_years.xml'
-    db.insert_espi_xml(xml)
-    print(f"Filled database with data from {xml}")
+    # db = EnergyHistory(path='/test/data/energy_history_test.db')
+    # xml = f'{PROJECT_PATH}/test/data/espi/espi_2_years.xml'
+    # db.insert_espi_xml(xml, 13371337)
+    # print(f"Filled database with data from {xml}")
 
 
 if __name__ == '__main__':
