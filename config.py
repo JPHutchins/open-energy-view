46d2bad1f3a6c29313f48e47062a0ede3833be2e
J.P. Hutchins <jphutchins@gmail.com>
2020-03-29 16:03:01 -0700

    Add config.py to secure secrets from env variables

---
+++ config.py
@@ -0,0 +1,32 @@
+from os import environ
+
+
+class Config:
+    """Base config vars."""
+
+    SECRET_KEY = environ.get("SECRET_KEY")
+    JWT_SECRET_KEY = environ.get("JWT_SECRET_KEY")
+    JWT_TOKEN_LOCATION = environ.get("JWT_TOKEN_LOCATION")
+    JWT_ACCESS_COOKIE_PATH = environ.get("JWT_ACCESS_COOKIE_PATH")
+    JWT_REFRESH_COOKIE_PATH = environ.get("JWT_REFRESH_COOKIE_PATH")
+    JWT_COOKIE_CSRF_PROTECT = True
+    JWT_CSRF_IN_COOKIES = True
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+
+class ProdConfig(Config):
+    DEBUG = False
+    TESTING = False
+    SQLALCHEMY_DATABASE_URI = environ.get("PROD_DATABASE_URI")
+    JWT_COOKIE_SECURE = True
+    JWT_ACCESS_TOKEN_EXPIRES = 900
+    JWT_REFRESH_TOKEN_EXPIRES = 2592000
+
+
+class DevConfig(Config):
+    DEBUG = True
+    TESTING = True
+    SQLALCHEMY_DATABASE_URI = environ.get("DEV_DATABASE_URI")
+    JWT_COOKIE_SECURE = False
+    JWT_ACCESS_TOKEN_EXPIRES = False
+    JWT_REFRESH_TOKEN_EXPIRES = False
