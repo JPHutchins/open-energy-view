476bce56fb94c98e883467e1e8d1ebf547d534ab
J.P. Hutchins <jphutchins@gmail.com>
2020-03-30 16:38:18 -0700

    Refactor demo DB creation

--- pgesmd/test_gui.py
+++ pgesmd/test_gui.py
@@ -3,19 +3,18 @@
 import os
 
 from pgesmd.database import EnergyHistory
+from . import create_app
 
 PROJECT_PATH = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 def fill_database():
     """Fill database using the test data."""
-    if os.path.exists(f'{PROJECT_PATH}/test/data/energy_history_test.db'):
-        os.remove(f'{PROJECT_PATH}/test/data/energy_history_test.db')
-    # db = EnergyHistory(path='/test/data/energy_history_test.db')
-    # xml = f'{PROJECT_PATH}/test/data/espi/espi_2_years.xml'
-    # db.insert_espi_xml(xml, 13371337)
-    # print(f"Filled database with data from {xml}")
+    create_app()
+    db = EnergyHistory(path="/test/data/energy_history_test.db")
+    xml = f"{PROJECT_PATH}/test/data/espi/espi_2_years.xml"
+    db.insert_espi_xml(xml, 13371337)
+    print(f"Filled database with data from {xml}")
 
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     fill_database()
