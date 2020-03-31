14d014337c22f06ee362db10b0c60ca040f0566e
J.P. Hutchins <jphutchins@gmail.com>
2020-03-30 20:42:39 -0700

    Implement tabs

--- pgesmd/frontend/js/components/SourceTabs.jsx
+++ pgesmd/frontend/js/components/SourceTabs.jsx
@@ -1,19 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 import EnergyHistory from "./EnergyHistory";
 
 const SourceTabs = props => {
-  const demoChart = <EnergyHistory source="PG&E" />;
-  const blankChart = <EnergyHistory source="None" />;
-  const tab = selected => {
-    if (selected) return <div className="tab-selected" />;
-    return <div className="tab-deselected" />;
+  const [nTabs, setNTabs] = useState(0); // set to props.(length of tab list) after testing
+  const [selectedTab, setSelectedTab] = useState(0); // always preselect first tab
+
+  const demoChart = <EnergyHistory source="PG&E" />; // test
+  const blankChart = <EnergyHistory source="None" />; //test
+
+  const testingList = [demoChart, blankChart]; // can remove after testing
+//   setNTabs(2); // can remove after testing
+
+  const handleClick = (e) => {
+      setSelectedTab(parseInt(e.currentTarget.getAttribute('value')))
+  }
+
+  const tab = tabIndex => {
+    if (tabIndex === selectedTab)
+      return (
+        <div
+          className="tab tab-selected"
+          value={tabIndex}
+          onClick={handleClick}
+        />
+      );
+    return <div className="tab" value={tabIndex} onClick={handleClick} />;
   };
 
   return (
     <div className="source-tabs">
-      {tab(true)}
-      {tab(false)}
-      {demoChart}
+      <div className="tab-container">
+        {tab(0)}
+        {tab(1)}
+        {tab(2)}
+        {tab(3)}
+      </div>
+      {testingList[selectedTab]}
     </div>
   );
 };
