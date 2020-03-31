1b7385c4c436eb02b7a35204c6ad882a648fd28f
J.P. Hutchins <jphutchins@gmail.com>
2020-03-30 19:24:27 -0700

    Add some test divs

--- pgesmd/frontend/js/components/SourceTabs.jsx
+++ pgesmd/frontend/js/components/SourceTabs.jsx
@@ -4,8 +4,18 @@ import EnergyHistory from "./EnergyHistory";
 const SourceTabs = props => {
   const demoChart = <EnergyHistory source="PG&E" />;
   const blankChart = <EnergyHistory source="None" />;
+  const tab = selected => {
+    if (selected) return <div className="tab-selected" />;
+    return <div className="tab-deselected" />;
+  };
 
-  return <div className="source-tabs"> {demoChart} </div>;
+  return (
+    <div className="source-tabs">
+      {tab(true)}
+      {tab(false)}
+      {demoChart}
+    </div>
+  );
 };
 
 export default SourceTabs;
