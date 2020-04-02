5242122abaaeeea6bf404293fcfb39be5d8d44b0
J.P. Hutchins <jphutchins@gmail.com>
2020-04-01 21:32:45 -0700

    Comment these functions

--- pgesmd/frontend/js/components/SourceTabs.jsx
+++ pgesmd/frontend/js/components/SourceTabs.jsx
@@ -1,6 +1,11 @@
 import React, { useState } from "react";
 import EnergyHistory from "./EnergyHistory";
 
+/**
+ * Returns a tabbed container. Each tab may contain components passed in as props (TODO).
+ *
+ * @param {Object} props Props.
+ */
 const SourceTabs = props => {
   const [nTabs, setNTabs] = useState(3); // set to props.(length of tab list) after testing
   const [selectedTab, setSelectedTab] = useState(0); // always preselect first tab
@@ -11,20 +16,25 @@ const SourceTabs = props => {
   const testingList = [demoChart, blankChart]; // can remove after testing
   //   setNTabs(2); // can remove after testing
 
+  /**
+   * Set the State Variable selectedTab to the value attribute of the click event.
+   *
+   * @param {Object} e Event.
+   */
   const handleClick = e => {
     setSelectedTab(parseInt(e.currentTarget.getAttribute("value")));
   };
 
+  /**
+   * Return a div with the className="tab" or className="tab-selected".
+   * The tab is selected if tabIndex matches the State Variable selectedTab.
+   *
+   * @param {Number} tabIndex The index of the tab, ex. 0, 1, 2, 3 etc.
+   */
   const tab = tabIndex => {
     if (tabIndex === selectedTab) {
       const last = tabIndex === nTabs ? "tab-selected-last" : "tab-selected";
-      return (
-        <div
-          className={last}
-          value={tabIndex}
-          onClick={handleClick}
-        />
-      );
+      return <div className={last} value={tabIndex} onClick={handleClick} />;
     }
     return <div className="tab" value={tabIndex} onClick={handleClick} />;
   };
