83ef8339b032bdd485e8835daea1c35d86ec871f
J.P. Hutchins <jphutchins@gmail.com>
2020-05-10 15:37:40 -0700

    Update filename

--- pgesmd/frontend/js/components/SourceRegistration.jsx
+++ pgesmd/frontend/js/components/SourceRegistration.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import cookie from "react-cookies";
 import { Form, Button, DropdownButton, Dropdown } from "react-bootstrap";
-import addPgeSource from "../service/AddSourceService";
+import { addPgeSource } from "../service/DatabaseService";
 
 const SourceRegistration = (props) => {
   const [form, setForm] = useState("");
