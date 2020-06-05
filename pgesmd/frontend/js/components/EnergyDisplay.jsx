9883a5ea73a2d5bac5132fc0dd5a47542bb3e389
J.P. Hutchins <jphutchins@gmail.com>
2020-06-04 18:59:10 -0700

    Remove old imports

--- pgesmd/frontend/js/components/EnergyDisplay.jsx
+++ pgesmd/frontend/js/components/EnergyDisplay.jsx
@@ -1,37 +1,5 @@
 import React, { useState } from "react";
 import EnergyChart from "./EnergyChart";
-import moment from "moment";
-import { EnergyHistory } from "../data-structures/EnergyHistory";
-import {
-  curry,
-  compose,
-  addIndex,
-  map,
-  chain,
-  join,
-  chian,
-  ap,
-  indexOf,
-  lift,
-  either,
-  sum,
-  isNil,
-  slice,
-  mean,
-  range,
-  isEmpty,
-  prepend,
-  reduce,
-  drop,
-  head,
-  pipe,
-  zip,
-  zipObj,
-  zipWith,
-  __,
-} from "ramda";
-import { Maybe, IO, Either, Identity } from "ramda-fantasy";
-import { startOfDay, endOfDay } from "date-fns";
 
 const EnergyDisplay = (props) => {
   const [test, setTest] = useState(props.tester);
