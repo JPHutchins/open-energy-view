9dad4be7a2011b877870373c23036333edaf9b0a
J.P. Hutchins <jphutchins@gmail.com>
2020-06-05 19:46:50 -0700

    Refactor makeFillWindow to abstract and clarify function purpose

--- pgesmd/frontend/js/functions/makeFillWindow.js
+++ pgesmd/frontend/js/functions/makeFillWindow.js
@@ -7,18 +7,50 @@ import { Either } from "ramda-fantasy";
  * @param {Number} window
  */
 export const makeFillWindow = (window) => (originalArr) => (f) => (arr) => {
-  // Re factor into multiple functions and compose.
-  const length = arr.length;
+  const output = centerOffset(window, arr);
+  return fillEndOfArray(window, originalArr, f, arr, output);
+};
+
+/**
+ * Remove the offset caused by a rolling operation from the dataset.
+ *
+ * Given a window size, window, that was used to generate a rolling operation on
+ * an array and the array, arr, resulting from that rolling operation, shift the
+ * contents of the array "left" by one half the window size and slice the array
+ * at that point.
+ *
+ * @param {Number} window
+ * @param {Array} arr
+ */
+function centerOffset(window, arr) {
   const middle = Math.floor(window / 2);
   const output = addIndex(map)(
     (x, i) => (i < window ? arr[window] : x),
     arr
   ).slice(middle);
+  return output;
+}
 
-  const remainingWindow = length - output.length;
-  const windowRange = range(1, remainingWindow + 1).reverse();
-  for (let rWindow of windowRange) {
-    output.push(f(originalArr.slice(length - rWindow - middle)));
+/**
+ * Fill the last (window / 2) values of the array with the results of applying
+ * the function, f, to a shrinking window size.
+ * 
+ * @param {Number} window 
+ * @param {Array} originalArr 
+ * @param {Function} f 
+ * @param {Array} arr 
+ * @param {Array} output 
+ */
+function fillEndOfArray(window, originalArr, f, arr, output) {
+    const length = arr.length;
+    const middle = Math.floor(window / 2);
+  
+    const remainingWindow = length - output.length;
+    const windowRange = range(1, remainingWindow + 1).reverse();
+  
+    for (let rWindow of windowRange) {
+      const newWindow = originalArr.slice(length - rWindow - middle);
+      output.push(f(newWindow));
+    }
+    return Either.Right(output);
   }
-  return Either.Right(output);
-};
