f8a0299f70873e0ac5507be5903b8bbedbe04151
J.P. Hutchins <jphutchins@gmail.com>
2020-06-20 20:01:11 -0700

    Aw, back to the old color scheme

--- pgesmd/frontend/js/data-structures/helpers/defaultPartitions.js
+++ pgesmd/frontend/js/data-structures/helpers/defaultPartitions.js
@@ -1,5 +1,5 @@
 export const defaultPartitions = [
-  { name: "Night", start: 1, color: "#FF0000" },
-  { name: "Day", start: 7, color: "#00FF00" },
-  { name: "Evening", start: 18, color: "#0000FF" },
+  { name: "Night", start: 1, color: "#393984" },
+  { name: "Day", start: 7, color: "#65BDB3" },
+  { name: "Evening", start: 18, color: "#B765BD" },
 ];
