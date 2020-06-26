6ec9b8173a8f5beb91a4b4cbc7afaaccd77be6c9
J.P. Hutchins <jphutchins@gmail.com>
2020-06-25 21:53:23 -0700

    Zip total, active, and passive use together

--- pgesmd/frontend/js/data-structures/helpers/makeBarGraphData.js
+++ pgesmd/frontend/js/data-structures/helpers/makeBarGraphData.js
@@ -5,30 +5,33 @@ import { compose } from "ramda";
 
 export const makeBarGraphData = (database, passiveUse) => {
   //TODO: refactor - consider moving further up in composition?
-  console.log(database, passiveUse);
+  //Rename this function?
   if (!database || !passiveUse) return;
-  let zipped = List([]);
+  const zipped = [];
 
   const getFrom = (object) => (key) => object.get(key);
   const useKey = (key) => (object) => object.get(key);
 
   for (let i = 0; i < database.size; i++) {
-    let hour = database.get(i);
-    let day = getTime(startOfDay(new Date(hour.get("x"))));
+    const hour = database.get(i);
+    const day = getTime(startOfDay(new Date(hour.get("x"))));
+
+    const total = hour.get("y");
+
     let passive = compose(
       useKey("y"),
       getFrom(passiveUse),
       indexInDb(passiveUse)
     )(day);
 
-    passive = passive < hour.get("y") ? passive : hour.get("y");
-    let active = hour.get("y") - passive
+    passive = passive < total ? passive : total;
+    const active = total - passive;
 
-    let zip = hour.set("passive", passive);
-    zip = zip.set("y", active);
-    zip = zip.set("total", hour.get("y"))
-
-    zipped = zipped.set(i, zip);
+    const zip = hour.withMutations((hour) => {
+      hour.set("passive", passive).set("y", active).set("total", hour.get("y"));
+    });
+   
+    zipped.push(zip)
   }
-  return zipped;
+  return List(zipped);
 };
