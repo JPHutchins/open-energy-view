30188bdf3f9bd278ee4cb1bf0a06767d72d819b5
J.P. Hutchins <jphutchins@gmail.com>
2020-06-28 22:26:42 -0700

    Switch to night at 0 (comfort)

--- pgesmd/frontend/js/data-structures/helpers/defaultPartitions.js
+++ pgesmd/frontend/js/data-structures/helpers/defaultPartitions.js
@@ -1,5 +1,5 @@
 export const defaultPartitions = [
-  { name: "Night", start: 1, color: "hsl(240, 40%, 37%)" },
+  { name: "Night", start: 0, color: "hsl(240, 40%, 37%)" },
   { name: "Day", start: 7, color: "hsl(173, 40%, 57%)" },
   { name: "Evening", start: 18, color: "hsl(296, 40%, 57%)" },
 ];
