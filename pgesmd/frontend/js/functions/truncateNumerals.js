37d3c1bc7cef81cd73390fccf020b590652192c1
J.P. Hutchins <jphutchins@gmail.com>
2020-06-28 22:25:56 -0700

    If there are 0 decimals, round before truncating

--- pgesmd/frontend/js/functions/truncateNumerals.js
+++ pgesmd/frontend/js/functions/truncateNumerals.js
@@ -4,5 +4,8 @@
 export const truncateNumerals = (numerals) => (number) => {
   const places = Math.floor(Math.log10(number));
   const decimalPlaces = Math.max(0, numerals - places - 1);
+  if (decimalPlaces === 0) {
+      number = Math.round(number)
+  }
   return number.toFixed(decimalPlaces);
 };
