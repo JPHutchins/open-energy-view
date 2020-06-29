147c3131611d32feaa7f811a3bb9ceaf543bdf9b
J.P. Hutchins <jphutchins@gmail.com>
2020-06-28 19:07:26 -0700

    Account for incomplete chunks in window to window comparison

--- pgesmd/frontend/js/components/ViewTotal.jsx
+++ pgesmd/frontend/js/components/ViewTotal.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import { differenceInHours } from "date-fns";
+import { min } from "ramda";
 
 const ViewTotal = ({ energyHistory }) => {
-  const windowMean =
-    energyHistory.hourlyMean *
-    differenceInHours(energyHistory.endDate, energyHistory.startDate);
+  const smallerWindowSize = Math.min(
+    differenceInHours(energyHistory.endDate, energyHistory.startDate),
+    differenceInHours(energyHistory.endDate, energyHistory.firstDate),
+    differenceInHours(energyHistory.lastDate, energyHistory.startDate)
+  );
+  const windowMean = energyHistory.hourlyMean * smallerWindowSize;
   const percent = Math.round(
     (energyHistory.windowData.windowSum / windowMean - 1) * 100
   );
