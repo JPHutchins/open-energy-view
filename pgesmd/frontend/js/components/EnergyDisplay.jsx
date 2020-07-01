52e58f70daca1f61b0850cfd193aa9ad6e3d2c7a
J.P. Hutchins <jphutchins@gmail.com>
2020-06-30 21:31:55 -0700

    Add TopBar component

--- pgesmd/frontend/js/components/EnergyDisplay.jsx
+++ pgesmd/frontend/js/components/EnergyDisplay.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import EnergyChart from "./EnergyChart";
+import TopBar from "./TopBar";
 import LowerBar from "./LowerBar";
 import RightBar from "./RightBar";
 
@@ -10,29 +11,15 @@ const EnergyDisplay = ({ energyHistoryInstance }) => {
     <div className="energy-history">
       <div className="energy-history-main-div">
         <div className="energy-chart">
+          <TopBar energyHistory={energyHistory} />
           <EnergyChart energyHistory={energyHistory} />
+          <LowerBar
+            energyHistory={energyHistory}
+            setEnergyHistory={setEnergyHistory}
+          />
         </div>
-        <RightBar
-          energyHistory={energyHistory}
-          /*database={this.database}
-          data={this.state.data.datasets}
-          sum={this.getSum()}
-          avg={this.getAllTimeAvg()}
-          carbonMultiplier={this.state.carbonMultiplier}
-          yoy={this.getYoyChange()}
-          pieData={this.getPieData()}
-          pieOptions={this.getPieOptions(this.getPieData())}
-          defaultValue={this.state.partPieView}
-          handlePartPieView={this.handlePartPieView}
-          selectedPartPieView={this.state.partPieView}
-          range={this.state.range}*/
-        />
+        <RightBar energyHistory={energyHistory}/>
       </div>
-
-      <LowerBar
-        energyHistory={energyHistory}
-        setEnergyHistory={setEnergyHistory}
-      />
     </div>
   );
 };
---
+++ pgesmd/frontend/js/components/TopBar.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import { format } from "date-fns";
+
+const TopBar = ({ energyHistory }) => {
+  const formatDate = (energyHistory) => {
+    if (energyHistory.custom)
+      return `${format(
+        energyHistory.startDate,
+        "EEEE, MMMM do, yyyy"
+      )} - ${format(energyHistory.endDate, "EEEE, MMMM do, yyyy")}`;
+    switch (energyHistory.windowData.windowSize) {
+      case "day":
+        return format(energyHistory.startDate, "EEEE, MMMM do, yyyy");
+      case "week":
+        return `Week of ${format(energyHistory.startDate, "MMMM do, yyyy")}`;
+      case "month":
+        return format(energyHistory.startDate, "MMMM yyyy");
+      case "year":
+        return format(energyHistory.startDate, "yyyy");
+      case "complete":
+        return `${format(energyHistory.startDate, "MMM do, yyyy")} - ${format(
+          energyHistory.endDate,
+          "MMMM do, yyyy"
+        )}`;
+      default:
+        return `${format(energyHistory.startDate, "MMM do, yy")} - ${format(
+          energyHistory.endDate,
+          "MMMM do, yy"
+        )}`;
+    }
+  };
+
+  return (
+    <div>
+      <div id="window-date">{formatDate(energyHistory)}</div>
+    </div>
+  );
+};
+export default TopBar;
