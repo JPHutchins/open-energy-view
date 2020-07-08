3251651de27c359559b535af0aa78db079e235dc
J.P. Hutchins <jphutchins@gmail.com>
2020-07-07 18:53:06 -0700

    Remove old tables

--- pgesmd/models.py
+++ pgesmd/models.py
@@ -56,25 +56,14 @@ class PgeSmd(db.Model):
     cert_crt_path = db.Column(db.String(100), nullable=True)
     cert_key_path = db.Column(db.String(100), nullable=True)
     # Information about the dataset
-    max_watt_hour = db.Column(db.Integer, default=0)
-    first_entry = db.Column(db.Integer, default=4102444799)
     last_entry = db.Column(db.Integer, default=0)
-    n_parts = db.Column(db.Integer, nullable=True)
-    part_values = db.Column(db.Integer, nullable=True)
     last_update = db.Column(db.Integer, default=0)
     # Options
     partition_options = db.Column(db.String(1000), nullable=True)
     # Relationship to data tables
-    raw = db.relationship("Raw", backref="pgesmd", lazy=True)
     espi = db.relationship("Espi", backref="pgesmd", lazy=True)
-    part = db.relationship("Part", backref="pgesmd", lazy=True)
-    day = db.relationship("Day", backref="pgesmd", lazy=True)
-    week = db.relationship("Week", backref="pgesmd", lazy=True)
-    month = db.relationship("Month", backref="pgesmd", lazy=True)
-    year = db.relationship("Year", backref="pgesmd", lazy=True)
-    daily_passive = db.relationship("DailyPassive", backref="pgesmd", lazy=True)
     __table_args__ = (db.UniqueConstraint(
-        "friendly_name", "partition_options",),)
+        "friendly_name", "u_id",),)
 
     def save_to_db(self) -> None:
         """Add the account to the TABLE pgesmd."""
@@ -89,18 +78,6 @@ class PgeSmd(db.Model):
         )
 
 
-class Raw(db.Model):
-    """Model for raw data from PGESMD API that is not hourly."""
-
-    id = db.Column(db.Integer, primary_key=True)
-    pge_id = db.Column(db.Integer, db.ForeignKey("pgesmd.id"), nullable=False)
-    # Data
-    start = db.Column(db.Integer, nullable=False)
-    duration = db.Column(db.Integer, nullable=False)
-    watt_hours = db.Column(db.Integer, nullable=False)
-    __table_args__ = (db.UniqueConstraint("pge_id", "start"),)
-
-
 class Espi(db.Model):
     """Model for ESPI data from PGESMD API."""
 
@@ -111,117 +88,3 @@ class Espi(db.Model):
     duration = db.Column(db.Integer, nullable=False)
     watt_hours = db.Column(db.Integer, nullable=False)
     __table_args__ = (db.UniqueConstraint("pge_id", "start",),)
-
-
-class Part(db.Model):
-    """Model for partition data from PGESMD API."""
-
-    id = db.Column(db.Integer, primary_key=True)
-    pge_id = db.Column(db.Integer, db.ForeignKey("pgesmd.id"), nullable=False)
-    # Data
-    start = db.Column(db.Integer, nullable=False)
-    part_avg = db.Column(db.Integer, nullable=False)
-    part_sum = db.Column(db.Integer, nullable=False)
-    # TEMP PATCH FOR OLD SCHEMA
-    end = db.Column(db.Integer, nullable=False)
-    middle = db.Column(db.Integer, nullable=False)
-    date = db.Column(db.Text, nullable=False)
-    part_type = db.Column(db.Integer, nullable=False)
-    __table_args__ = (db.UniqueConstraint("pge_id", "start",),)
-
-
-class Day(db.Model):
-    """Model for daily data from PGESMD API."""
-
-    id = db.Column(db.Integer, primary_key=True)
-    pge_id = db.Column(db.Integer, db.ForeignKey("pgesmd.id"), nullable=False)
-    # Data
-    start = db.Column(db.Integer, nullable=False)
-    day_avg = db.Column(db.Integer, nullable=False)
-    day_sum = db.Column(db.Integer, nullable=False)
-    min = db.Column(db.Integer, nullable=False)
-    baseline = db.Column(db.Integer, nullable=True)
-    # TEMP PATCH FOR OLD SCHEMA
-    date = db.Column(db.Text, nullable=False)
-    end = db.Column(db.Integer, nullable=False)
-    middle = db.Column(db.Integer, nullable=False)
-    date = db.Column(db.Text, nullable=False)
-    __table_args__ = (db.UniqueConstraint("pge_id", "start",),)
-
-
-class Week(db.Model):
-    """Model for weekly data from PGESMD API."""
-
-    id = db.Column(db.Integer, primary_key=True)
-    pge_id = db.Column(db.Integer, db.ForeignKey("pgesmd.id"), nullable=False)
-    # Data
-    start = db.Column(db.Integer, nullable=False)
-    week_avg = db.Column(db.Integer, nullable=False)
-    week_sum = db.Column(db.Integer, nullable=False)
-    # TEMP PATCH FOR OLD SCHEMA
-    end = db.Column(db.Integer, nullable=False)
-    middle = db.Column(db.Integer, nullable=False)
-    date = db.Column(db.Text, nullable=False)
-    __table_args__ = (db.UniqueConstraint("pge_id", "start",),)
-
-
-class Month(db.Model):
-    """Model for monthly data from PGESMD API."""
-
-    id = db.Column(db.Integer, primary_key=True)
-    pge_id = db.Column(db.Integer, db.ForeignKey("pgesmd.id"), nullable=False)
-    # Data
-    start = db.Column(db.Integer, nullable=False)
-    month_avg = db.Column(db.Integer, nullable=False)
-    month_sum = db.Column(db.Integer, nullable=False)
-    # TEMP PATCH FOR OLD SCHEMA
-    end = db.Column(db.Integer, nullable=False)
-    middle = db.Column(db.Integer, nullable=False)
-    date = db.Column(db.Text, nullable=False)
-    __table_args__ = (db.UniqueConstraint("pge_id", "start",),)
-
-
-class Year(db.Model):
-    """Model for yearly data from PGESMD API."""
-
-    id = db.Column(db.Integer, primary_key=True)
-    pge_id = db.Column(db.Integer, db.ForeignKey("pgesmd.id"), nullable=False)
-    # Data
-    start = db.Column(db.Integer, nullable=False)
-    year_avg = db.Column(db.Integer, nullable=False)
-    year_sum = db.Column(db.Integer, nullable=False)
-    # TEMP PATCH FOR OLD SCHEMA
-    end = db.Column(db.Integer, nullable=False)
-    middle = db.Column(db.Integer, nullable=False)
-    date = db.Column(db.Text, nullable=False)
-    __table_args__ = (db.UniqueConstraint("pge_id", "start",),)
-
-
-class DailyPassive(db.Model):
-    """Model for the daily passive statistic."""
-
-    id = db.Column(db.Integer, primary_key=True)
-    pge_id = db.Column(db.Integer, db.ForeignKey("pgesmd.id"), nullable=False)
-    # Data
-    start = db.Column(db.Integer, nullable=False)
-    watt_hours = db.Column(db.Integer, nullable=False)
-    __table_args__ = (db.UniqueConstraint("pge_id", "start",),)
-
-
-class RevokedToken(db.Model):
-    """Model for the revoked tokens (user logout)."""
-
-    __tablename__ = "revoked_tokens"
-    id = db.Column(db.Integer, primary_key=True)
-    jti = db.Column(db.String(120))
-
-    def add(self):
-        """Add the token to the blacklist."""
-        db.session.add(self)
-        db.session.commit()
-
-    @classmethod
-    def is_jti_blacklisted(cls, jti) -> bool:
-        """Return True if token is blacklisted, else False."""
-        query = cls.query.filter_by(jti=jti).first()
-        return bool(query)
