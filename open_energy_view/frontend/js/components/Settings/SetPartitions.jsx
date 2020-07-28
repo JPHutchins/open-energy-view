886635e7af28348398316f8b49c544be90d08ce5
J.P. Hutchins <jphutchins@gmail.com>
2020-07-27 21:53:29 -0700

    Use react-compound-slider to implement "partition editor" for the user

---
+++ open_energy_view/frontend/js/components/Settings/SetPartitions.jsx
@@ -0,0 +1,147 @@
+import React from "react";
+import { Slider, Rail, Handles, Tracks, Ticks } from "react-compound-slider";
+import { SliderRail, Handle, Track, Tick } from "./slider";
+import { useState } from "react";
+import { idToIndex } from "./functions";
+import { to12Hour } from "../../functions/to12Hour";
+
+// TODO: display all-time day average as preview?
+
+const sliderStyle = {
+  position: "relative",
+  width: "100%",
+};
+const domain = [0, 23];
+
+const SetPartitions = ({ existingPartitionOptions, restrictView }) => {
+  const [currentHandle, setCurrentHandle] = useState(null);
+  const [edit, hookSetEdit] = useState(null);
+  const [partitions, setPartitions] = useState(existingPartitionOptions.value);
+
+  const addPartition = (afterIndex) => {
+    const updatedPartitions = Object.assign([], partitions);
+    updatedPartitions.splice(afterIndex + 1, 0, {
+      name: `Period ${afterIndex + 2}`,
+      start: partitions[afterIndex].start + 1,
+      color: "#D3D3D3",
+    });
+    setPartitions(updatedPartitions);
+  };
+
+  const removePartition = (index) => {
+    if (partitions.length === 1) return;
+    const updatedPartitions = Object.assign([], partitions);
+    updatedPartitions.splice(index, 1);
+    if (updatedPartitions.length === 1) {
+      updatedPartitions[0].start = 0;
+    }
+    setPartitions(updatedPartitions);
+  };
+
+  const updatePartitions = (prop, newValue, handleId) => {
+    const partitionIndex = idToIndex(handleId);
+    const updatedPartition = {
+      ...partitions[partitionIndex],
+      [prop]: newValue,
+    };
+    const updatedPartitions = Object.assign([], partitions);
+    updatedPartitions[partitionIndex] = updatedPartition;
+    setPartitions(updatedPartitions);
+  };
+
+  const updateStartPoints = (newStartPoints) => {
+    const updatedPartitions = Object.assign([], partitions).map((x, i) => ({
+      ...x,
+      start: newStartPoints[i],
+    }));
+    if (updatedPartitions.length === 1) {
+      updatedPartitions[0].start = 0;
+    }
+    setPartitions(updatedPartitions);
+  };
+
+  const setEdit = (id) => {
+    hookSetEdit(id === edit ? null : id);
+  };
+
+  // Adapted from react-compound-slider demos by Steve Hall
+  // https://github.com/sghall/react-compound-slider
+
+  return (
+    <div>
+      <Slider
+        mode={2}
+        step={1}
+        domain={domain}
+        rootStyle={sliderStyle}
+        onChange={updateStartPoints}
+        values={partitions.map((x) => x.start)}
+      >
+        <Rail>
+          {({ getRailProps }) => (
+            <SliderRail
+              getRailProps={getRailProps}
+              color={partitions[partitions.length - 1].color}
+            />
+          )}
+        </Rail>
+        <Handles>
+          {({ handles, getHandleProps }) => (
+            <div className="slider-handles">
+              {handles.map((handle) => (
+                <Handle
+                  key={handle.id}
+                  handle={handle}
+                  domain={domain}
+                  getHandleProps={getHandleProps}
+                  updatePartitions={updatePartitions}
+                  partitions={partitions}
+                  name={partitions[idToIndex(handle.id)].name}
+                  currentHandle={currentHandle}
+                  setCurrentHandle={setCurrentHandle}
+                  edit={edit}
+                  setEdit={setEdit}
+                  addPartition={addPartition}
+                  removePartition={removePartition}
+                />
+              ))}
+            </div>
+          )}
+        </Handles>
+        <Tracks left={false} right={false}>
+          {({ tracks, getTrackProps }) => (
+            <div className="slider-tracks">
+              {tracks.map(({ id, source, target }) => (
+                <Track
+                  key={id}
+                  source={source}
+                  target={target}
+                  getTrackProps={getTrackProps}
+                  color={partitions[idToIndex(id)].color}
+                />
+              ))}
+            </div>
+          )}
+        </Tracks>
+        <Ticks count={24}>
+          {({ ticks }) => (
+            <div className="slider-ticks">
+              {ticks.map((tick) => (
+                <Tick
+                  key={tick.id}
+                  tick={tick}
+                  count={ticks.length}
+                  format={to12Hour}
+                />
+              ))}
+            </div>
+          )}
+        </Ticks>
+      </Slider>
+      <button onClick={() => restrictView(null, partitions)}style={{ marginTop: "80px" }}>
+        Save and Reload with Updated Time Periods
+      </button>
+    </div>
+  );
+};
+export default SetPartitions;
---
+++ open_energy_view/frontend/js/components/Settings/slider.jsx
@@ -0,0 +1,418 @@
+// Adapted from react-compound-slider demos by Steve Hall
+// https://github.com/sghall/react-compound-slider
+
+import React, { Fragment } from "react";
+import PropTypes from "prop-types";
+import ColorPicker from "./ColorPicker";
+import { idToIndex } from "./functions";
+
+// *******************************************************
+// RAIL
+// *******************************************************
+const railOuterStyle = {
+  position: "absolute",
+  width: "100%",
+  height: 42,
+  transform: "translate(0%, -50%)",
+  cursor: "pointer",
+};
+
+const railInnerStyle = (color) => ({
+  position: "absolute",
+  width: "100%",
+  height: 14,
+  transform: "translate(0%, -50%)",
+  pointerEvents: "none",
+  backgroundColor: color,
+});
+
+export function SliderRail({ getRailProps, color }) {
+  return (
+    <Fragment>
+      <div style={railOuterStyle} {...getRailProps()} />
+      <div style={railInnerStyle(color)} />
+    </Fragment>
+  );
+}
+
+const hoverHandlePopup = (
+  percent,
+  setEdit,
+  setCurrentHandle,
+  addPartition,
+  id
+) => (
+  <>
+    <div
+      style={{
+        left: `${percent}%`,
+        position: "absolute",
+        transform: "translate(-16px, 40px)",
+        transformOrigin: "bottom left",
+        zIndex: 14,
+        padding: "5px",
+        backgroundColor: "#5f5566",
+        color: "white",
+        cursor: "pointer",
+        paddingLeft: 10,
+        paddingRight: 10,
+      }}
+      onClick={() => {
+        setEdit(id);
+        setCurrentHandle(null);
+      }}
+    >
+      Edit
+    </div>
+    <div
+      style={{
+        left: `${percent}%`,
+        position: "absolute",
+        transform: "translate(32px, 40px)",
+        transformOrigin: "bottom left",
+        zIndex: 14,
+        padding: "5px",
+        backgroundColor: "#5f5566",
+        color: "white",
+        cursor: "pointer",
+        paddingLeft: 10,
+        paddingRight: 10,
+      }}
+      onClick={() => {
+        addPartition(idToIndex(id));
+      }}
+    >
+      Add
+    </div>
+  </>
+);
+
+const editMenu = (
+  percent,
+  setEdit,
+  updatePartitions,
+  id,
+  partitions,
+  removePartition,
+  name
+) => (
+  <div
+    style={{
+      left: `${Math.min(
+        percent,
+        ((window.innerWidth - 250) / window.innerWidth) * 100
+      )}%`,
+      position: "absolute",
+      transform: "translate(0px, 45px)",
+      zIndex: 15,
+      width: 240,
+      height: "auto",
+      border: "solid",
+      borderColor: "#5f5566",
+      backgroundColor: "white",
+    }}
+  >
+    <div
+      style={{
+        position: "absolute",
+        right: 5,
+        top: 5,
+        width: 20,
+        height: 20,
+        border: "solid",
+        borderColor: "#5f5566",
+        cursor: "pointer",
+      }}
+      onClick={() => setEdit(null)}
+    />
+    <div style={{ margin: "0 auto", width: 225 }}>
+      <input
+        style={{
+          width: "auto",
+          margin: "10px auto 10px auto",
+          border: "solid",
+          borderColor: "#5f5566",
+          textAlign: "center",
+          backgroundColor: "transparent",
+        }}
+        id="changeName"
+        type="text"
+        placeholder={name || ""}
+        onChange={(e) => updatePartitions("name", e.currentTarget.value, id)}
+      />
+    </div>
+    <div
+      style={{
+        width: "225px",
+        margin: "0 auto",
+      }}
+    >
+      <ColorPicker
+        handleId={id}
+        updatePartitions={updatePartitions}
+        partitions={partitions}
+      />
+    </div>
+    <div
+      style={{
+        width: "auto",
+        margin: "10px auto 10px auto",
+        border: "solid",
+        textAlign: "center",
+        color: "white",
+        backgroundColor: "#5f5566",
+        cursor: "pointer",
+      }}
+      onClick={(e) => {
+        e.stopPropagation();
+        removePartition(idToIndex(id));
+        setEdit(null);
+      }}
+    >
+      Remove This Period
+    </div>
+  </div>
+);
+
+SliderRail.propTypes = {
+  getRailProps: PropTypes.func.isRequired,
+};
+
+// *******************************************************
+// HANDLE COMPONENT
+// *******************************************************
+export function Handle({
+  domain: [min, max],
+  handle: { id, value, percent },
+  disabled,
+  getHandleProps,
+  updatePartitions,
+  partitions,
+  name,
+  currentHandle,
+  setCurrentHandle,
+  edit,
+  setEdit,
+  addPartition,
+  removePartition,
+}) {
+  return (
+    <Fragment>
+      <div
+        onMouseEnter={() => setCurrentHandle(id)}
+        onMouseLeave={() => setCurrentHandle(null)}
+        style={{
+          left: `${percent}%`,
+          position: "absolute",
+          transform: "translate(-50%, -50%)",
+          WebkitTapHighlightColor: "rgba(0,0,0,0)",
+          zIndex: 5,
+          width: 28,
+          height: 42,
+          cursor: "pointer",
+          backgroundColor: "none",
+        }}
+        {...getHandleProps(id)}
+      >
+        {id === currentHandle &&
+          hoverHandlePopup(
+            percent,
+            setEdit,
+            setCurrentHandle,
+            addPartition,
+            id
+          )}
+      </div>
+
+      <div
+        role="slider"
+        aria-valuemin={min}
+        aria-valuemax={max}
+        aria-valuenow={value}
+        style={{
+          left: `${percent}%`,
+          position: "absolute",
+          transform: "translate(-50%, -50%)",
+          zIndex: 2,
+          width: 24,
+          height: 24,
+          backgroundColor: disabled ? "#666" : "#ffffff",
+          border: "solid",
+          borderColor: "#5f5566",
+        }}
+      />
+      <div
+        role="current-time-period-label"
+        aria-valuenow={name}
+        style={{
+          left: `${percent}%`,
+          position: "absolute",
+          transform: "translate(4px, -32px) rotate(-45deg) ",
+          transformOrigin: "bottom left",
+          zIndex: 15,
+        }}
+      >
+        {name}
+      </div>
+      {id === edit &&
+        editMenu(
+          percent,
+          setEdit,
+          updatePartitions,
+          id,
+          partitions,
+          removePartition,
+          name
+        )}
+    </Fragment>
+  );
+}
+
+Handle.propTypes = {
+  domain: PropTypes.array.isRequired,
+  handle: PropTypes.shape({
+    id: PropTypes.string.isRequired,
+    value: PropTypes.number.isRequired,
+    percent: PropTypes.number.isRequired,
+  }).isRequired,
+  getHandleProps: PropTypes.func.isRequired,
+  disabled: PropTypes.bool,
+};
+
+Handle.defaultProps = {
+  disabled: false,
+};
+
+// *******************************************************
+// KEYBOARD HANDLE COMPONENT
+// Uses a button to allow keyboard events
+// *******************************************************
+export function KeyboardHandle({
+  domain: [min, max],
+  handle: { id, value, percent },
+  disabled,
+  getHandleProps,
+}) {
+  return (
+    <button
+      role="slider"
+      aria-valuemin={min}
+      aria-valuemax={max}
+      aria-valuenow={value}
+      style={{
+        left: `${percent}%`,
+        position: "absolute",
+        transform: "translate(-50%, -50%)",
+        zIndex: 2,
+        width: 24,
+        height: 24,
+        borderRadius: "50%",
+        backgroundColor: disabled ? "#666" : "#ffc400",
+      }}
+      {...getHandleProps(id)}
+    />
+  );
+}
+
+KeyboardHandle.propTypes = {
+  domain: PropTypes.array.isRequired,
+  handle: PropTypes.shape({
+    id: PropTypes.string.isRequired,
+    value: PropTypes.number.isRequired,
+    percent: PropTypes.number.isRequired,
+  }).isRequired,
+  getHandleProps: PropTypes.func.isRequired,
+  disabled: PropTypes.bool,
+};
+
+KeyboardHandle.defaultProps = {
+  disabled: false,
+};
+
+// *******************************************************
+// TRACK COMPONENT
+// *******************************************************
+export function Track({ source, target, getTrackProps, disabled, color }) {
+  return (
+    <div
+      style={{
+        position: "absolute",
+        transform: "translate(0%, -50%)",
+        height: 14,
+        zIndex: 1,
+        backgroundColor: disabled ? "#999" : color,
+        borderRadius: 7,
+        cursor: "pointer",
+        left: `${source.percent}%`,
+        width: `${target.percent - source.percent}%`,
+      }}
+      {...getTrackProps()}
+    />
+  );
+}
+
+Track.propTypes = {
+  source: PropTypes.shape({
+    id: PropTypes.string.isRequired,
+    value: PropTypes.number.isRequired,
+    percent: PropTypes.number.isRequired,
+  }).isRequired,
+  target: PropTypes.shape({
+    id: PropTypes.string.isRequired,
+    value: PropTypes.number.isRequired,
+    percent: PropTypes.number.isRequired,
+  }).isRequired,
+  getTrackProps: PropTypes.func.isRequired,
+  disabled: PropTypes.bool,
+};
+
+Track.defaultProps = {
+  disabled: false,
+};
+
+// *******************************************************
+// TICK COMPONENT
+// *******************************************************
+export function Tick({ tick, count, format }) {
+  return (
+    <div>
+      <div
+        style={{
+          position: "absolute",
+          marginTop: 14,
+          width: 1,
+          height: 5,
+          backgroundColor: "rgb(200,200,200)",
+          left: `${tick.percent}%`,
+        }}
+      />
+      <div
+        style={{
+          position: "absolute",
+          marginTop: 22,
+          fontSize: 10,
+          textAlign: "center",
+          marginLeft: `${-(100 / count) / 2}%`,
+          width: `${100 / count}%`,
+          left: `${tick.percent}%`,
+        }}
+      >
+        {format(tick.value)}
+      </div>
+    </div>
+  );
+}
+
+Tick.propTypes = {
+  tick: PropTypes.shape({
+    id: PropTypes.string.isRequired,
+    value: PropTypes.number.isRequired,
+    percent: PropTypes.number.isRequired,
+  }).isRequired,
+  count: PropTypes.number.isRequired,
+  format: PropTypes.func.isRequired,
+};
+
+Tick.defaultProps = {
+  format: (d) => d,
+};
