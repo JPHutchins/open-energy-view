6642583d71550fdba65ee1f1677e5bf12fc3e5c2
J.P. Hutchins <jphutchins@gmail.com>
2020-08-04 05:35:10 +0000

    Remove pgesmd_self_access imports, setup demo user if doesn't exist

--- open_energy_view/__init__.py
+++ open_energy_view/__init__.py
@@ -7,15 +7,8 @@ from flask_bcrypt import Bcrypt
 from flask_jwt_extended import JWTManager
 from xml.etree import cElementTree as ET
 from sqlalchemy import exc
-from pgesmd_self_access.helpers import (
-    parse_espi_data,
-    get_auth_file,
-    get_bulk_id_from_xml,
-)
-from pgesmd_self_access.api import SelfAccessApi
 
 PROJECT_PATH = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
 
 db = SQLAlchemy()
 rest = Api()
@@ -75,63 +68,18 @@ def create_app() -> Flask:
 
     with app.app_context():
         db.create_all()
-
-        @app.route("/pgesmd", methods=["POST"])
-        def handle_pgesmd_post():
-            # legacy for Self Access API testing
-            data = request.data
-            try:
-                resource_uri = ET.fromstring(data)[0].text
-            except ET.ParseError:
-                # print(f'Could not parse message: {data}')
-                return f"Could not parse message: {data}", 500
-            except IndexError as e:
-                print(e, data)
-                auth = get_auth_file(f"{PROJECT_PATH}/auth/auth.json")
-                api = SelfAccessApi(*auth)
-                api.request_latest_data()
-                return f'No index "0" in parsed XML: {data}', 500
-
-            auth = get_auth_file(f"{PROJECT_PATH}/auth/auth.json")
-            api = SelfAccessApi(*auth)
-
-            xml = api.get_espi_data(resource_uri)
-            bulk_id = get_bulk_id_from_xml(xml)
-            source_id = (
-                db.session.query(models.Source)
-                .filter_by(provider_id=bulk_id)
-                .first()
-                .id
+        # Setup the demo user and data
+        demo_user = (
+            db.session.query(models.User).filter_by(email="jph@demo.com").first()
+        )
+        if not demo_user:
+            demo_user = models.User(
+                email="jph@demo.com",
+                password=bcrypt.generate_password_hash("demo").decode("utf-8"),
             )
-
-            data_update = []
-            last_entry = ""
-            for entry in parse_espi_data(xml):
-                data_update.append(
-                    {
-                        "source_id": source_id,
-                        "start": entry[0],
-                        "duration": entry[1],
-                        "watt_hours": entry[2],
-                    }
-                )
-                last_entry = entry[0]
             try:
-                db.session.bulk_insert_mappings(models.Espi, data_update)
-                db.session.commit()
-            except exc.IntegrityError:
-                db.session.rollback()
-                db.engine.execute(
-                    "INSERT OR IGNORE INTO espi (source_id, start, duration, watt_hours) VALUES (:source_id, :start, :duration, :watt_hours)",
-                    data_update,
-                )
-            finally:
-                timestamp = int(time() * 1000)
-                source_row = db.session.query(models.Source).filter_by(id=source_id)
-                source_row.update({"last_entry": last_entry})
-                source_row.update({"last_update": timestamp})
-                db.session.commit()
-
-            return {}, 200
+                demo_user.save_to_db()
+            except Exception as e:
+                print(e)
 
         return app
