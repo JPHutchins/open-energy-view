de78c56a6f2afaff220639a7341cd314fd2b4ff1
J.P. Hutchins <jphutchins@gmail.com>
2020-08-18 16:07:25 -0700

    wsgi uses import from package level now

--- wsgi.py
+++ wsgi.py
@@ -1,6 +1,9 @@
 from open_energy_view import create_app
+from os import environ
+from open_energy_view import config
 
-app = create_app()
+
+app = create_app(config.DevConfig)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
