c3d9ef0fd1631c27018a9ac35acffc86c96cd780
J.P. Hutchins <jphutchins@gmail.com>
2020-08-18 16:06:51 -0700

    Celery config

---
+++ open_energy_view/celery.py
@@ -0,0 +1,26 @@
+from celery import Celery
+
+
+celery = Celery(
+    "tasks", backend="rpc://", broker="amqp://jp:admin@localhost:5672/myvhost",
+)
+
+
+celery.conf.update(
+    {
+        "imports": (
+            "open_energy_view.celery_tasks",
+            "open_energy_view.espi_helpers",
+            "open_energy_view.utility_apis"
+        ),
+        "task_routes": {
+            "get_jp": {"queue": "io"},
+            "process_data": {"queue": "cpu"},
+            "fake_fetch": {"queue": "io"},
+            "insert_espi_xml_into_db": {"queue": "cpu"},
+        },
+        "task_serializer": "json",
+        "result_serializer": "json",
+        "accept_content": ["json"],
+    }
+)
