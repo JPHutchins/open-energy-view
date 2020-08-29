0f2bb656a0dd08746120854bcbbd9ce54f20fba5
J.P. Hutchins <jphutchins@gmail.com>
2020-08-29 16:20:01 -0700

    Clarify privacy and instructions on utility add

--- open_energy_view/frontend/js/components/SourceRegistration.jsx
+++ open_energy_view/frontend/js/components/SourceRegistration.jsx
@@ -10,6 +10,9 @@ const SourceRegistration = ({ history, restrictView }) => {
   const [clientId, setClientId] = useState("");
   const [clientSecret, setClientSecret] = useState("");
   const [utility, setUtility] = useState("Utility Company");
+  const [displayCustomSourceOption, setDisplayCustomSourceOption] = useState(
+    false
+  );
 
   // TODO: server will respond 500 on failing UniqueConstraint for
   // "Provider ID" (third party ID) or name - update UI on promise reject
@@ -53,50 +56,47 @@ const SourceRegistration = ({ history, restrictView }) => {
   );
 
   const selfAccess = (
-    <div className="register-box">
-      <Form style={{ maxWidth: "450px" }} onSubmit={handleSubmit}>
-        <h4>Add Custom Source </h4>
-        <div style={{ fontSize: "8pt", color: "gray" }}>
-          This is mostly unimplemented. After adding a source below (enter 555
-          for Third-Party ID, Client ID, and Client Secret), you can upload your
-          own ESPI XML from the new resource's Settings page.
-        </div>
-        <Form.Group controlId="formPge">
-          <Form.Label>Name</Form.Label>
-          <Form.Control
-            className="login-form"
-            type="text"
-            placeholder="Name, like PG&E or Home PG&E"
-            onChange={(e) => setName(e.target.value)}
-          />
-          <Form.Label>Third-Party ID</Form.Label>
-          <Form.Control
-            className="login-form"
-            type="text"
-            placeholder="Third-Party ID"
-            onChange={(e) => setThirdPartyId(e.target.value)}
-          />
-          <Form.Label>Client ID</Form.Label>
-          <Form.Control
-            className="login-form"
-            type="text"
-            placeholder="Client ID"
-            onChange={(e) => setClientId(e.target.value)}
-          />
-          <Form.Label>Client Secret</Form.Label>
-          <Form.Control
-            className="login-form"
-            type="text"
-            placeholder="Client Secret"
-            onChange={(e) => setClientSecret(e.target.value)}
-          />
-        </Form.Group>
-        <Button variant="primary" type="submit">
-          Add Source
-        </Button>
-        <hr />
-      </Form>
-    </div>
+    <Form style={{ maxWidth: "450px" }} onSubmit={handleSubmit}>
+      <div style={{ fontSize: "8pt", color: "gray" }}>
+        This is mostly unimplemented. After adding a source below (enter 555 for
+        Third-Party ID, Client ID, and Client Secret), you can upload your own
+        ESPI XML from the new resource's Settings page.
+      </div>
+      <Form.Group controlId="formPge">
+        <Form.Label>Name</Form.Label>
+        <Form.Control
+          className="login-form"
+          type="text"
+          placeholder="Name, like PG&E or Home PG&E"
+          onChange={(e) => setName(e.target.value)}
+        />
+        <Form.Label>Third-Party ID</Form.Label>
+        <Form.Control
+          className="login-form"
+          type="text"
+          placeholder="Third-Party ID"
+          onChange={(e) => setThirdPartyId(e.target.value)}
+        />
+        <Form.Label>Client ID</Form.Label>
+        <Form.Control
+          className="login-form"
+          type="text"
+          placeholder="Client ID"
+          onChange={(e) => setClientId(e.target.value)}
+        />
+        <Form.Label>Client Secret</Form.Label>
+        <Form.Control
+          className="login-form"
+          type="text"
+          placeholder="Client Secret"
+          onChange={(e) => setClientSecret(e.target.value)}
+        />
+      </Form.Group>
+      <Button variant="primary" type="submit">
+        Add Source
+      </Button>
+      <hr />
+    </Form>
   );
 
   return (
@@ -104,14 +104,17 @@ const SourceRegistration = ({ history, restrictView }) => {
       <div className="register-box">
         <Form style={{ maxWidth: "450px" }} onSubmit={handleUtilityOAuth}>
           <Form.Group controlId="formPgeOAuth">
-            <h3>Add Your Utility</h3>
-            <div style={{ fontSize: "10pt", color: "gray" }}>
-              Authorize your utility company to share your home energy data. You
-              will need the login information for your utilities. It is the same
-              login that you would use to pay your bill online.
-            </div>
+            <h3>Add Your Utilities</h3>
             <hr />
             <h4>Select your utility company</h4>
+            <div style={{ fontSize: "10pt", color: "gray" }}>
+              If your utility company is not listed below please visit{" "}
+              <a href="https://github.com/JPHutchins/open-energy-view">
+                the Github
+              </a>{" "}
+              and open an issue requesting integration of your utility.
+            </div>
+            <br />
             <DropdownButton title={utility} onSelect={setUtility}>
               <Dropdown.Item eventKey="Pacific Gas & Electric">
                 Pacific Gas & Electric
@@ -121,14 +124,47 @@ const SourceRegistration = ({ history, restrictView }) => {
               </Dropdown.Item>
             </DropdownButton>
             <hr />
-            <h4>Link your account</h4>
-            <Button variant="primary" type="submit">
+            <h4>Link your accounts</h4>
+            <div style={{ fontSize: "10pt", color: "gray" }}>
+              Click "Authorize" to go to your utility company's login page where
+              you will confirm that you want to grant Open Energy View access to
+              your energy usage data.
+            </div>
+            <br />
+            <Button
+              variant="primary"
+              type="submit"
+              style={{ width: "100%" }}
+              disabled={utility === "Utility Company"}
+            >
               Authorize
             </Button>
+            <hr />
+            <h4>Privacy</h4>
+            <div style={{ fontSize: "10pt", color: "gray" }}>
+              Your utility company may provide Open Energy View with your
+              service addresses. We will not store these addresses on our
+              servers but we will display them to you to help you to identify
+              and name your meters. Please{" "}
+              <a href="/#/privacy" target="_blank">
+                click here to read our Privacy Policy
+              </a>
+              .
+            </div>
           </Form.Group>
         </Form>
       </div>
-      {selfAccess}
+      <div className="register-box">
+        <h4
+          onClick={() =>
+            setDisplayCustomSourceOption(!displayCustomSourceOption)
+          }
+          className="hover-pointer"
+        >
+          Add Custom Source{" "}
+        </h4>
+        {displayCustomSourceOption && selfAccess}
+      </div>
     </>
   );
 };
